dc3f5e2ef795902f7c5cb1d7b176a1e42c5505d5
krzysmak <krzysmak@onet.pl>
2023-01-13 21:21:47 +0100

    Dodane materiały z samouczka (nie działa)

---
+++ Triangle.h
@@ -0,0 +1,68 @@
+#ifndef TRIANGLE
+#define TRIANGLE
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <dxgi1_6.h>
+#include <d3d12.h>
+#include <Windows.h>
+#include <wrl.h>
+#include <string>
+#include <DirectXMath.h>
+#include <exception>
+
+using namespace DirectX;
+using namespace Microsoft::WRL;
+
+class D3D12HelloTriangle
+{
+public:
+    D3D12HelloTriangle(UINT width, UINT height, std::wstring name);
+
+    virtual void OnInit();
+    virtual void OnUpdate();
+    virtual void OnRender();
+    virtual void OnDestroy();
+
+private:
+    static const UINT FrameCount = 2;
+
+    struct Vertex
+    {
+        XMFLOAT3 position;
+        XMFLOAT4 color;
+    };
+
+    // Pipeline objects.
+    D3D12_VIEWPORT m_viewport;
+    D3D12_RECT m_scissorRect;
+    ComPtr<IDXGISwapChain3> m_swapChain;
+    ComPtr<ID3D12Device> m_device;
+    ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
+    ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+    ComPtr<ID3D12CommandQueue> m_commandQueue;
+    ComPtr<ID3D12RootSignature> m_rootSignature;
+    ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+    ComPtr<ID3D12PipelineState> m_pipelineState;
+    ComPtr<ID3D12GraphicsCommandList> m_commandList;
+    UINT m_rtvDescriptorSize;
+
+    // App resources.
+    ComPtr<ID3D12Resource> m_vertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+
+    // Synchronization objects.
+    UINT m_frameIndex;
+    HANDLE m_fenceEvent;
+    ComPtr<ID3D12Fence> m_fence;
+    UINT64 m_fenceValue;
+
+    void LoadPipeline();
+    void LoadAssets();
+    void PopulateCommandList();
+    void WaitForPreviousFrame();
+};
+
+#endif // !TRIANGLE
