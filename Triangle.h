db2325f2aad8c42705365ce7cda6dcd0c5560d2e
krzysmak <krzysmak@onet.pl>
2023-01-16 17:48:31 +0100

    pozbycie się DXSample

--- Triangle.h
+++ Triangle.h
@@ -5,13 +5,20 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
-#include "DXSample.h"
-#include "stdafx.h"
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include "d3dx12.h"
+#include <string>
+#include <wrl.h>
+#include <shellapi.h>
 
 using namespace DirectX;
 using namespace Microsoft::WRL;
 
-class D3D12HelloTriangle : public DXSample
+class D3D12HelloTriangle
 {
 public:
     D3D12HelloTriangle(UINT width, UINT height, std::wstring name);
--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-#include "stdafx.h"
 #include "Triangle.h"
 
-_Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
     D3D12HelloTriangle sample(1280, 720, L"D3D12 Hello Triangle");
